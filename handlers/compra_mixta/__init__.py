ce43234a6ef4d74d3b1e371df881d91826e1f0b3
sofiaqsy <keyl.qsy@gmail.com>
2025-06-03 19:29:00 -0500

    Refactorizar compra_mixta en módulos: Crear __init__.py

--- handlers/compra_mixta/__init__.py
+++ handlers/compra_mixta/__init__.py
@@ -1,3 +1,8 @@
+"""
+Paquete para manejar la funcionalidad de compra mixta.
+Refactorizado para mejorar mantenibilidad y corregir errores.
+"""
+
 from handlers.compra_mixta.handler import register_compra_mixta_handlers
 
 __all__ = ['register_compra_mixta_handlers']
